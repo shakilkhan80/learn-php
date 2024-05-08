7794ee481327f4057c362ae26620d267e177ea1c
shakilkhan80 <shakilkhan81198@gmail.com>
2024-05-08 09:10:04 +0600

    composer added to the project

--- public/index.php
+++ public/index.php
@@ -3,20 +3,15 @@
 use Core\Session;
 use Core\ValidationException;
 
-session_start();
 
 const BASE_PATH = __DIR__ . '/../';
 
+require BASE_PATH . 'vendor/autoload.php';
+
+session_start();
+
 require BASE_PATH . 'Core/functions.php';
 
-spl_autoload_register(function ($class) {
-
-
-    $result = str_replace('\\', '/', $class);
-
-    require base_path("{$result}.php");
-
-});
 
 require base_path('bootstrap.php');
 
